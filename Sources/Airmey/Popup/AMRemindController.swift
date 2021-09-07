7f484bddc56748fd0f44080dcfe07829ab1f05df
fuhuachuan <fuhuachuan@clipclaps.com>
2021-09-07 14:43:59 +0800

    fix issue

--- Sources/Airmey/Popup/AMRemindController.swift
+++ Sources/Airmey/Popup/AMRemindController.swift
@@ -35,7 +35,7 @@ open class AMRemindController: AMPopupController ,AMRemindable{
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    public override func viewDidLoad() {
+    open override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .clear
         self.view.addSubview(self.blurView)
--- Sources/Airmey/Popup/AMWaitController.swift
+++ Sources/Airmey/Popup/AMWaitController.swift
@@ -51,7 +51,7 @@ open class AMWaitController: AMPopupController,AMWaitable {
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    public override func viewDidLoad() {
+    open override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .clear
         self.view.addSubview(self.blurView)
