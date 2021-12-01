08c779725408be496123d2647b0919fc3c60da91
yongjun chen <chenyongjun0@yeah.net>
2021-12-01 15:04:22 +0800

    fix bug

--- Sources/Airmey/Widget/AMTextView.swift
+++ Sources/Airmey/Widget/AMTextView.swift
@@ -8,47 +8,90 @@
 import UIKit
 
 public class AMTextView: UITextView {
-    /// 占位文字
-    public var placeholder: String?
-    /// 占位文字颜色
-    public var placeholderColor: UIColor? = UIColor.lightGray
+    /// setNeedsDisplay调用drawRect
+    public var placeholder: String = "" {
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
+    
+    var placeHolderColor: UIColor = UIColor.gray{
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
+    
+    public override var font: UIFont?{
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
+    
+    public override var text: String!{
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
+    
+    public override var attributedText: NSAttributedString!{
+        didSet{
+            self.setNeedsDisplay()
+        }
+    }
     
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
-        // 设置默认字体
-        self.font = UIFont.systemFont(ofSize: 15)
-        // 使用通知监听文字改变
-        NotificationCenter.default.addObserver(self, selector: #selector(textDidChange(_:)), name: UITextView.textDidChangeNotification, object: nil)
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
+        /// default字号
+        self.font = UIFont.systemFont(ofSize: 14)
+        NotificationCenter.default.addObserver(self, selector: #selector(textDidChanged(noti:)), name: UITextView.textDidChangeNotification, object: self)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public override func draw(_ rect: CGRect) {
-        // 如果有文字,就直接返回,不需要画占位文字
-        if self.hasText {
-            return
-        }
-        
-        // 属性
-        let attrs: [NSAttributedString.Key : Any] = [NSAttributedString.Key.foregroundColor: self.placeholderColor as Any, NSAttributedString.Key.font: self.font!]
-        
-        // 文字
-        var rect1 = rect
-        rect1.origin.x = 5
-        rect1.origin.y = 8
-        rect1.size.width = rect1.size.width - 2*rect1.origin.x
-        (self.placeholder! as NSString).draw(in: rect1, withAttributes: attrs)
-    }
-    
-    @objc func textDidChange(_ note: Notification) {
-        // 会重新调用drawRect:方法
+    @objc func textDidChanged(noti: NSNotification)  {
         self.setNeedsDisplay()
     }
     
+    public override func draw(_ rect: CGRect) {
+        if self.hasText {
+            return
+        }
+        var newRect = CGRect()
+        newRect.origin.x = 5
+        newRect.origin.y = 7
+        let size = self.placeholder.getStringSize(rectSize: rect.size, font: self.font ?? UIFont.systemFont(ofSize: 14))
+        newRect.size.width = size.width
+        newRect.size.height = size.height
+        /// 将placeHolder画在textView上
+        (self.placeholder as NSString).draw(in: newRect, withAttributes: [NSAttributedString.Key.font: self.font ?? UIFont.systemFont(ofSize: 14),NSAttributedString.Key.foregroundColor: self.placeHolderColor])
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        self.setNeedsDisplay()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UITextView.textDidChangeNotification, object: self)
+    }
+    
+}
+
+
+extension String {
+    /// 计算字符串的尺寸
+    ///
+    /// - Parameters:
+    ///   - text: 字符串
+    ///   - rectSize: 容器的尺寸
+    ///   - fontSize: 字体
+    /// - Returns: 尺寸
+    ///
+    public func getStringSize(rectSize: CGSize,font: UIFont) -> CGSize {
+        let str: NSString = self as NSString
+        let rect = str.boundingRect(with: rectSize, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return CGSize(width: ceil(rect.width), height: ceil(rect.height))
+    }
 }
